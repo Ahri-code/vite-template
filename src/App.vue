256ddc9b48022b2074790bcdf012a1b86188db84
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-30 17:11:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import HeaderApp from './components/HeaderApp.vue'
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
+import { store } from './data/store'
+import axios from 'axios'
+
+export default {
+    name: "App",
+    components: {
+        HeaderApp,
+        MainApp,
+        FooterApp
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <HeaderApp />
+    </header>
+    <main>
+        <MainApp />
+    </main>
+    <footer>
+        <FooterApp />
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use './style/specific/utility.scss' as *;
+@use './style/specific/style.scss' as *;
+</style>
